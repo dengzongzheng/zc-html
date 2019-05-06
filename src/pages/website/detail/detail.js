e68a55e095f4ec8e5fde8dbcb768de534631ea21
zori0 <776021342@qq.com>
2019-05-06 21:34:00 +0800

    init

--- src/pages/website/detail/detail.js
+++ src/pages/website/detail/detail.js
@@ -6,46 +6,6 @@ import Footer from '@components/footer/footer';
 import xhr from '@/service/xhr/index';
 import {imgPath} from "@/service/xhr/config";
 
-function RenderDetail(props) {
-    const goods = props.goods;
-    if(!goods.productImages){
-        return(<div/>);
-    }
-    const items = goods.productImages.map((item,index)=>
-        <li className="img-hover" key={index}>
-            <img alt="" src={imgPath+item} width="50" height="64" />
-        </li>
-    )
-
-    return(
-        <div className="detail-content-box">
-            <div className="left-box">
-                <img src={imgPath+goods.productImages[0]} className="detail-goods-img"/>
-                <div className="spec-list" clstag="shangpin|keycount|product|lunbotu_2">
-                    <a id="spec-forward" href="javascript:;" className="arrow-prev disabled">
-                        <i className="sprite-arrow-prev"></i>
-                    </a>
-                    <a id="spec-backward" href="javascript:;" className="arrow-next disabled">
-                        <i className="sprite-arrow-next"></i></a>
-                    <div id="spec-list" className="spec-items">
-                        <ul className="lh">
-                            {items}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div className="right-box">
-                <div className="right-content">
-                    <div className="detail-title">{goods.productName}</div>
-                    <div className="desc">{goods.direction}</div>
-                </div>
-            </div>
-            <div className="clear"/>
-        </div>
-    )
-}
-
 export default class WebsiteDetail extends Component{
 
     constructor(props){
@@ -53,7 +13,8 @@ export default class WebsiteDetail extends Component{
         this.state = {
             goods:{
 
-            }
+            },
+            currentImag:""
         };
     }
 
@@ -68,19 +29,63 @@ export default class WebsiteDetail extends Component{
         xhr.get('/api/detail',param).then(function (data) {
             if(data.code=="1"){
                 that.setState(state=>({
-                    goods:data.data
+                    goods:data.data,
+                    currentImag:data.data.productImages[0]
                 }))
             }
         });
     }
 
+    changImag(item){
+        console.log(item);
+        this.setState(state=>({
+            currentImag:item
+        }));
+    }
+
     render() {
         const goods = this.state.goods;
+        const currentImg = this.state.currentImag;
+        if(!goods.productImages){
+            return(<div/>);
+        }
+        const items = goods.productImages.map((item,index)=>
+            <li className="img-hover" key={index} onClick={()=>this.changImag(item)}>
+                <img alt="" src={imgPath+item} width="50" height="64" />
+            </li>
+        );
+        const detailData = (
+            <div className="detail-content-box">
+                <div className="left-box">
+                    <img src={imgPath+currentImg} className="detail-goods-img"/>
+                    <div className="spec-list" clstag="shangpin|keycount|product|lunbotu_2">
+                        <a id="spec-forward" href="javascript:;" className="arrow-prev disabled">
+                            <i className="sprite-arrow-prev"/>
+                        </a>
+                        <a id="spec-backward" href="javascript:;" className="arrow-next disabled">
+                            <i className="sprite-arrow-next"/></a>
+                        <div id="spec-list" className="spec-items">
+                            <ul className="lh">
+                                {items}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="right-box">
+                    <div className="right-content">
+                        <div className="detail-title">{goods.productName}</div>
+                        <div className="desc">{goods.direction}</div>
+                    </div>
+                </div>
+                <div className="clear"/>
+            </div>
+        );
         return(
             <div>
                 <Header/>
                 <Nav/>
-                <RenderDetail goods={goods}></RenderDetail>
+               {detailData}
                 <Footer/>
             </div>
         );
